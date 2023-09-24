88588e8c7f22f2dc097daa06b9ea1efd778688d3
Confuzzl <fuzzltheduzzl@gmail.com>
2023-09-24 00:13:22 -0400

    image grid and structure change

--- generator.js
+++ generator.js
@@ -1,27 +1,21 @@
-const folderLookup = {
-    "home": "index",
-    "favorites": "favorite",
-    "how to": "skills",
-    "nyc": "spots"
-}
-
 const fs = require("fs");
 
-pageDirectories = fs.readdirSync(__dirname).filter(file => fs.lstatSync(file).isDirectory() && file !== ".git");
+pageDirectories = fs.readdirSync(`${__dirname}\\app`).filter(file => fs.lstatSync(`${__dirname}\\app\\${file}`).isFile());
 
-for (const [index, page] of pageDirectories.entries()) {
-    console.log(page, folderLookup[page]);
-    fs.writeFile(`${page}\\${folderLookup[page]}.html`, `<!DOCTYPE html>
+for (const page of pageDirectories) {
+    let name = page.replace(".html", "");
+    fs.writeFile(`app\\${page}`,
+        `<!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script defer src="../main.js" onload="generateBoilerPlate('${folderLookup[page]}')"></script>
+    <script defer src="scripts/template.js" onload="generateBoilerPlate('${name}')"></script>
 </head>
-
-<body onload="generateNavigationBar('${folderLookup[page]}')">
-
+        
+<body onload="generateNavigationBar('${name}')">
+        
 </body>
 
 </html>`, (err) => { if (err) return console.log("error"); });
--- home/script.js
+++
@@ -1,3 +0,0 @@
-function writeMain() {
-    console.log("aa");
-}
